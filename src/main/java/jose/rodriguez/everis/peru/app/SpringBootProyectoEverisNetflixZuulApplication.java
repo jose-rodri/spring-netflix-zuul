acb21663453fb376910d5d63c831c76cf5e2a0ec
JoseR <rodri@gmail.com>
2019-09-25 17:56:12 -0500

    Archivo modificado en rutas

--- src/main/java/jose/rodriguez/everis/peru/app/SpringBootProyectoEverisNetflixZuulApplication.java
+++ src/main/java/jose/rodriguez/everis/peru/app/SpringBootProyectoEverisNetflixZuulApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import jose.rodriguez.everis.peru.app.errorFilter.ErrorFilter;
+import jose.rodriguez.everis.peru.app.portFilter.PostFilter;
+import jose.rodriguez.everis.peru.app.preFilter.PreFilter;
+import jose.rodriguez.everis.peru.app.routeFilter.RouteFilter;
 
 @EnableZuulProxy 
 @EnableDiscoveryClient 
@@ -13,5 +18,23 @@ public class SpringBootProyectoEverisNetflixZuulApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProyectoEverisNetflixZuulApplication.class, args);
 	}
-
+/*
+	
+	@Bean
+    public PreFilter preFilter() {
+        return new PreFilter();
+    }
+    @Bean
+    public PostFilter postFilter() {
+        return new PostFilter();
+    }
+    @Bean
+    public ErrorFilter errorFilter() {
+        return new ErrorFilter();
+    }
+    @Bean
+    public RouteFilter routeFilter() {
+        return new RouteFilter();
+    }
+    */
 }
---
+++ src/main/java/jose/rodriguez/everis/peru/app/errorFilter/ErrorFilter.java
@@ -0,0 +1,30 @@
+package jose.rodriguez.everis.peru.app.errorFilter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+
+public class ErrorFilter  extends ZuulFilter{
+
+  
+
+  @Override
+  public String filterType() {
+      return "error";
+  }
+
+  @Override
+  public int filterOrder() {
+      return 1;
+  }
+
+  @Override
+  public boolean shouldFilter() {
+      return true;
+  }
+
+  @Override
+  public Object run() throws ZuulException {
+      System.out.println("Inside Error Filter");
+      return null;
+  }
+}
